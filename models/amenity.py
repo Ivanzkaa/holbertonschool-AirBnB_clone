39552f1db6416c38b79afd53b5141c148f9f6715
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-13 17:24:18 -0400

    class inherited from the Basemodel Amenity

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a class named 'amenity'"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Inherits from the basemodel"""
+    name = ""
