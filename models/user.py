79bd28fae9a7653c884c7dfa0b4d4ca831cf2da6
Ivanzkaa <ivanzka@me.com>
2022-10-13 09:50:22 -0400

    adding the users

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""""
+
+
+import email
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
