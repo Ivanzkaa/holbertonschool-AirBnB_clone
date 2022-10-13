ef5b25f0f239a9c58ef9fb328f69c3f6ea51dcd6
Ivanzkaa <ivanzka@me.com>
2022-10-13 09:51:12 -0400

    updating with the user

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,11 +4,12 @@
 
 import json
 from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
     """Initializing the Filestorage class"""
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
@@ -18,7 +19,7 @@ class FileStorage:
     def new(self, obj):
         """sets in the object to match the name"""
         cls_name = type(obj).__name__
-        key_obj = "{} {}".format(cls_name, obj.id)
+        key_obj = "{}.{}".format(cls_name, obj.id)
         self.__objects[key_obj] = obj
 
     def save(self):
@@ -29,10 +30,10 @@ class FileStorage:
             json.dump(obj_dict, f)
         
     def reload(self):
-        """"""
+        """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, "r", encoding="UTF8") as f:
-                obj_dir = json.loads(f.read())
+            with open(self.__file_path, 'r', encoding="UTF8") as fd:
+                obj_dir = json.loads(fd.read())
                 for key, value in obj_dir.items():
                     self.__objects[key] = BaseModel(**value)
         except:
