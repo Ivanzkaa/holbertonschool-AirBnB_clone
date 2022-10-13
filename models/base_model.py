1510ec572c15d609841da1cd4e46d4df2b9f7dcb
Ivanzkaa <ivanzka@me.com>
2022-10-13 12:00:00 -0400

    updating the model/save storage method

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage(self)
 
     def save(self):
         """updating the public instance with the current\
