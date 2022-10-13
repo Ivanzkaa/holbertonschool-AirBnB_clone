a09816cd0574d4b72e0d8badb2da67bfd092d554
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-10-13 17:25:41 -0400

    Class inherited from the BaseModel State

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define the class named 'state'"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Inherits from the basemodel file"""
+    name = ""
